1b3a7abc8e7b37fc662911b0edcafa393b382844
2147229MCA <95898970+2147229MCA@users.noreply.github.com>
2022-02-18 14:45:48 +0530

    Add files via upload

--- Auctio/RegVal.js
+++ Auctio/RegVal.js
@@ -2,57 +2,139 @@ const email = document.getElementById("GetEmail");
 const phNum = document.getElementById("GetPh");
 const nam = document.getElementById("GetName");
 const psw = document.getElementById("GetPsw");
-const regname = /^\D*$/;
-const regex1 = /^([a-z0-9\.-]+)@(auctio+)\.([a-z]{1,8})$/;
-const regex2 = /^([a-z0-9\.-]+)@(Auctio+)\.([a-z]{1,8})(.[a-z]{1,8})$/;
-const regtendigit = /^\d{10}$/;
-const regpsw = /^([a-zA-Z])(?=.*?[a-z])(?=.*?[#?!@$%^&*-\[\]\{\}<>`+_;:\=])(?=.*?[a-zA-z0-9]).{6,}$/;
-let cname = "";
-let cpsw = "";
-let cphone = "";
-let cemail = "";
- 
-function valAll() {
-    console.log("validating all the fiels");
-    if (!(nam.value.search(regname) == 0 && nam.value != "")) {
-      alert("Invalid user name");
-    } else {
-      if (!regtendigit.test(phNum.value)) {
-        alert("Invalid phone number");
-      } else {
-        if (!(regex1.test(email.value) || regex2.test(email.value))) {
-          alert("Invalid Email id");
-        } else {
-          if (!regpsw.test(psw.value)) {
-            alert("password creteria is not met");
-          } else {
-            document.cookies = "username="+nam.value+";"
-            +"email="+email.value+";"
-            +"phone="+phNum.value+";"
-            +"password="+psw.value+";"
-            +" expires=Fri, 18 Feb 2022 12:00:00 UTC";
+const age = document.getElementById("GetAge");
+const usernam = document.getElementById("GetUName");
+const er = document.getElementsByClassName("erorDis");
 
-            cname = document.cookies.split(";")[0].split("=")[1];
-            cemail = document.cookies.split(";")[1].split("=")[1];
-            cphone = document.cookies.split(";")[2].split("=")[1];
-            cpsw = document.cookies.split(";")[3].split("=")[1];
+
+const contnum = /^\d*$/;
+const regname = /^\D*$/;
+const regex1 = /^([a-z0-9\.-]+)@([a-z]+)\.in$/;
+const regtendigit = /^\d{11}$/;
+const regpsw =
+  /^([a-zA-Z])(?=.*?[a-z])(?=.*?[#?!@$%^&*-\[\]\{\}<>`+_;:\=])(?=.*?[a-zA-z0-9]).{8,}$/;
+
+
+
+
+nam.onkeydown = (event) => {
+  
+  
+    if (nam.value.search(regname) == 0 && nam.value != "") {
+      er[0].innerText = "valid name";
+      er[0].style.color = "lime";
+    } else {
+      er[0].innerText = "invalid name For example use John Smith";
+      er[0].style.color = "red";
+    }
+  };
+
+
+phNum.onkeydown = () => {
+  if (regtendigit.test(phNum.value)) {
+    er[1].innerText = "valid phone number";
+    er[1].style.color = "lime";
+  } else {
+    er[1].innerText = "invalid phone number Try 3974939999";
+    er[1].style.color = "red";
+  }
+};
+
+
+email.onkeydown = () => {
+  if (regex1.test(email.value)) {
+    er[3].innerText = "Your Email is Valid";
+    er[3].style.color = "lime";
+  } else {
+    er[3].innerText = "Invalid Email Id for example use Codec@codesatr.in";
+    er[3].style.color = "red";
+  }
+};
+
+
+age.onkeydown = () => {
+  
+  if (age.value > 0 && age.value <= 100) {
+    er[4].innerText = "valid age";
+    er[4].style.color = "lime";
+  } else {
+    er[4].innerText = "invalid age try giving 18";
+    er[4].style.color = "red";
+  }
+};
+
+
+usernam.onkeydown = (event) => {
+  if (usernam.value.search(contnum) == 0) {
+    let key = event.keyCode;
+    er[5].innerText = "don't enter numbers in this field";
+    er[5].style.color = "red";
+    return (key >= 65 && key <= 90) || key == 8;
+  } 
+  else {
+    if (usernam.value.search(regname) == 0 && usernam.value != "") {
+        
+    
+      usernam.value = usernam.value.charAt(0).toUpperCase() + usernam.value.substring(1);
+      er[5].innerText = "valid user name";
+      er[5].style.color = "lime";
+    } else {
+      er[5].innerText = "invalid user name ";
+      er[5].style.color = "red";
+    }
+  }
+};
+
+
+
+ psw.onkeydown = () => {
+    if (regpsw.test(psw.value)) {
+      er[2].innerText = "valid password";
+      er[2].style.color = "lime";
+    } else {
+      er[2].innerText = "invalid password For example Try Qqwe@#@77su";
+      er[2].style.color = "red";
+    }
+  };
+  
+
+
+
+function valAll() {
+  console.log("validating all the fiels");
+  if (!(nam.value.search(regname) == 0 && nam.value != "")) {
+    alert("Invalid name");
+  } 
+  else{
+    if (!(usernam.value.search(regname) == 0 && usernam.value != "")){
+        alert("Invalid user name");
+    }
+  else {
+    if (!regtendigit.test(phNum.value)) {
+      alert("Invalid phone number");
+    } else {
+      if (!regex1.test(email.value)) {
+        alert("Invalid Email id");
+      } else {
+        if (!regpsw.test(psw.value)) {
+          alert("password creteria is not met");
+        } else {
+            
 
             
-            if (confirm("Press ok if the details are correct\n"
-            +"User name"+" : "+cname+"\n"
-            +"Email"+" : "+cemail+"\n"
-            +"Phone"+" : "+cphone)) {
-                console.log(cname);
-                document.getElementById("tovalidate").action = "./index.html";
-                
-              } else {
-                location.reload();
-      
-}
-              
-         
+            document.cookies = "name="+nam.value+";"
+             +"email="+email.value+";"
+             +"phone="+phone.value+";"
+             +"username="+usernam.value+";"
+             +" expires=Thu, 10 Feb 2022 12:00:00 UTC";
+
+            
+            localStorage.setItem("phoneNumber", phNum.value);
+            localStorage.setItem("emailID", email.value);
+          
         }
       }
     }
   }
+}
 }
